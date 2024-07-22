0ddf2cb58a621868117b5bf8e7afe49e22666989
Wahyunianti <wahyunianti2002@gmail.com>
2024-07-23 00:21:57 +0700

    tambah rute autentikasi

---
+++ app/Http/Middleware/PenulisMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class PenulisMiddleware
+{
+    public function handle(Request $request, Closure $next): Response{
+        if(Auth::check()){
+            if(Auth::user()->role_id == 2) {
+                return $next($request);
+            }else{
+                return redirect('/')->with('message', 'access denied');
+            }
+        }else{
+            return redirect('/login')->with('message', 'Login sebagai admin untuk melakukan access');
+        }
+    }
+
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Role;
+
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
