d8722c70582dbcf7ec03d8497e94a7e19209ede2
Wahyunianti <wahyunianti2002@gmail.com>
2024-07-28 15:25:45 +0700

    bug fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -141,7 +141,7 @@
                         <div>
                             <label for="password"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password"
+                            <input type="password" name="password" placeholder="password"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                 required />
                         </div>
